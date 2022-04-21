aeb3916774c8dec667592496f91e88291bbcd918
alemayehuseyoum <alexman3b@gmail.com>
2022-04-21 01:19:33 -0700

    string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: concatinated string
+*/
+
+char *_strcat(char *dest, char *src)
+
+{
+return (strcat(dest, src));
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,1 @@
-# alx-low_level_programming
-# nested loops
 # 0x06. C - More pointers, arrays and strings
