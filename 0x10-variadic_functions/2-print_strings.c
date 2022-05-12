932e9ae1f982bea396141e3e21f889777e9f5e6f
alemayehuseyoum <alexman3b@gmail.com>
2022-05-12 05:51:35 -0700

    variadic function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			if (i < (n - 1) && separator != 0)
+			if (i < (n - 1) && separator != NULL)
 			{
 				printf("%s%s", str, separator);
 			}
