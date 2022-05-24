cc692e26b65d3ef2046be210b8852008d0ffd5b0
alemayehuseyoum <alexman3b@gmail.com>
2022-05-24 06:46:19 -0700

    list in t

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,12 +15,12 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		diff = head - head->next;
 		count++;
-		printf("[%p] %f\n", (void *)head, head->n);
+		printf("[%p] %i\n", (void *)head, head->n);
 		if (diff > 0)
 			head = head->next;
 		else
 		{
-			printf("-> [%p] %f\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %i\n", (void *)head->next, head->next->n);
 			break;
 		}
 
