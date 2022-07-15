53f7aba05913a785f53a798d7f788e7fc7854462
alemayehuseyoum <alexman3b@gmail.com>
2022-07-15 02:03:46 -0700

    shared

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: concatinated string
+*/
+
+char *_strcat(char *dest, char *src)
+
+{
+return (strcat(dest, src));
+}
